603dffde06eb42bbd88f548d6aa8391ad7e5df51
popman431 <leo431@naver.com>
2018-07-28 23:55:58 +0900

    edit Ethiopian_math Finished Algorithm.

--- Ethiopian_math.py
+++ Ethiopian_math.py
@@ -1,19 +1,35 @@
-# Thinking algorithm 1
-num1 = 13
-num2 = 12
-_sum =0
-for i in range(1,2+1):
-    if num1 % 2 == 1:
-        _sum = _sum + num2
-        num1 = int(num1 /2) 
-        num2 = num2 *2
-    elif num1 % 2 == 0:
-        _sum = _sum
-        num1 = int(num1 /2) 
-        num2 = num2 *2
-    elif num1 ==1:
-        _sum = num2 + _sum
-
-print(_sum)
+# Finish Algorithm
+num1 = int(input("Type num1 : "))
+num2 = int(input("Type num2 : "))
+_sum = 0
+while True:
+    if int(num1 / 2 ==0):
+        break
+    else:
+        if num1 % 2 == 1:
+            print(num1)
+            print(num2)
+            print(_sum)
+            print("\n")
+            _sum = _sum + num2
+            num1 = int(num1 /2) 
+            num2 = num2 *2
+            
+        elif num1 % 2 == 0:
+            print(num1)
+            print(num2)
+            print(_sum)
+            print("\n")
+            _sum = _sum
+            num1 = int(num1 /2) 
+            num2 = num2 *2
+         
+        elif num1 ==1:
+            print(num1)
+            print(num2)
+            print(_sum)
+            print("\n")
+            _sum = num2 + _sum
 print(num1)
 print(num2)
+print(_sum)
